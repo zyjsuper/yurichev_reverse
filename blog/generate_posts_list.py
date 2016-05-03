743ebc41f740fda8da7487e28c2348c1b3116b3d
Dennis Yurichev <dennis@yurichev.com>
2016-05-03 17:27:21 +0300

    new blog post

--- blog/generate_posts_list.py
+++ blog/generate_posts_list.py
@@ -26,6 +26,7 @@ def main():
 
     f2.write ("<table>\n")
 
+    post(f1,f2,datetime.datetime(2016, 5, 3,0,0,0), "XOR_mask_2","Simple encryption using XOR mask, part II")
     post(f1,f2,datetime.datetime(2016, 4,29,0,0,0), "XOR_mask_1","Simple encryption using XOR mask")
     post(f1,f2,datetime.datetime(2016, 4,22,0,0,0), "signed_division_using_shifts","Signed division using shifts")
     post(f1,f2,datetime.datetime(2016, 4,19,0,0,0), "lzhuf","Bug in LZHuf.c by Haruyasu Yoshizaki")
