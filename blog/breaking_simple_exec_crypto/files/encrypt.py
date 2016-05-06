72373ba05e3e3d75bb511160e2e3e119657a8f51
Dennis Yurichev <dennis@yurichev.com>
2016-05-06 18:50:33 +0300

    new blog post

---
+++ blog/breaking_simple_exec_crypto/files/encrypt.py
@@ -0,0 +1,37 @@
+import sys, hexdump, array
+
+def xor_strings(s,t):
+    # https://en.wikipedia.org/wiki/XOR_cipher#Example_implementation
+    """xor two strings together"""
+    return "".join(chr(ord(a)^ord(b)) for a,b in zip(s,t))
+
+IV=array.array('B', [147, 94, 252, 218, 38, 192, 199, 213, 225, 112, 143, 108, 10, 3, 128, 232]).tostring()
+
+def chunks(l, n):
+    n = max(1, n)
+    return [l[i:i + n] for i in range(0, len(l), n)]
+
+def read_file(fname):
+    file=open(fname, mode='rb')
+    content=file.read()
+    file.close()
+    return content
+
+def e(i, k):
+    return chr ((ord(i)+k) % 256)
+
+def encrypt(buf):
+    return e(buf[0], 0)+ e(buf[1], 1)+ e(buf[2], 2) + e(buf[3], 3)+ e(buf[4], 4)+ e(buf[5], 5)+ e(buf[6], 6)+ e(buf[7], 7)+ e(buf[8], 8)+ e(buf[9], 9)+ e(buf[10], 10)+ e(buf[11], 11)+ e(buf[12], 12)+ e(buf[13], 13)+ e(buf[14], 14)+ e(buf[15], 15)
+
+fout=open(sys.argv[2], mode='wb')
+
+prev=IV
+content=read_file(sys.argv[1])
+tmp=chunks(content, 16)
+for p in tmp:
+    c=encrypt(xor_strings (p, prev))
+    prev=xor_strings(c, p)
+    hexdump.hexdump(prev)
+    fout.write(c)
+fout.close()
+
