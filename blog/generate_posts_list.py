4de9c7c6ad988206592c9630803475265f36393b
Dennis Yurichev <dennis@yurichev.com>
2017-10-11 14:14:39 +0300

    new blog post: GCD

--- blog/generate_posts_list.py
+++ blog/generate_posts_list.py
@@ -26,6 +26,7 @@ def main():
 
     f2.write ("<table>\n")
 
+    post(f1,f2,datetime.datetime(2017,10,11,0,0,0), "GCD","Explanation of the Greatest Common Divisor using Z3 SMT solver, etc")
     post(f1,f2,datetime.datetime(2017,10, 7,0,0,0), "SAP","SAP cluster table unpacker")
     post(f1,f2,datetime.datetime(2017,10, 4,0,0,0), "sorting_network","Proving sorting network correctness using Z3 SMT solver")
     post(f1,f2,datetime.datetime(2017, 9,30,0,0,0), "XOR_SAT","Proving bizarre XOR alternative using SAT solver")
