f98f8f319a1cbb80dda4be127e8155a19c3a6152
Dennis Yurichev <dennis@yurichev.com>
2018-01-01 00:49:55 +0200

    new blog post

--- blog/generate_posts_list.py
+++ blog/generate_posts_list.py
@@ -26,6 +26,7 @@ def main():
 
     f2.write ("<table>\n")
 
+    post(f1,f2,datetime.datetime(2018, 1, 1,0,0,0), "ToySMT","ToySMT - simple SMT solver under ~1500 SLOC of pure C.")
     post(f1,f2,datetime.datetime(2017,12, 7,0,0,0), "menage","Ménage problem")
     post(f1,f2,datetime.datetime(2017,12, 5,0,0,0), "LCG_Z3","Can rand() generate 10 consecutive zeroes?")
     post(f1,f2,datetime.datetime(2017,12, 5,0,0,0), "modinv","Yet another explanation of modulo inverse")
